7ac246cb4a8112ecbe41f6be0af8346568d9f59b
Thomas Vermot-Gauchy <t.vermot-gauchy@happy-technologies.com>
2018-03-15 16:57:33 +0100

    create view && component

---
+++ test_vuecli/src/components/Form.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>{{ msg }}</div>
+    <form>
+      <div v-for="(field, key) in fields" v-bind:key="key">
+        name : {{ field.name }} -
+        type : {{ field.name }}
+        <textinput :tes="field.name"></textinput>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import textinput from './TextInput'
+
+export default {
+  name: 'formview',
+  components: {
+    textinput
+  },
+  data () {
+    return {
+      msg: 'FORM',
+      fields: [
+        {
+          name: 'YOLOOO',
+          type: 'text'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ test_vuecli/src/components/TextInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="field">
+    TEXT INPUT!!!
+    <label>{{ tes }}</label>
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'textinput',
+  data () {
+    return {
+    }
+  },
+  props: {
+    tes: String
+  }
+}
+</script>
